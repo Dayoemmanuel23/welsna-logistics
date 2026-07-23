3551f2d9926944d7e16525b2fd37260b0d6429fc
dayoemmanuelj <emmanueljimoh@gmail.com>
2026-07-23 21:45:15 +0100

    mail secure updated

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -12,7 +12,7 @@ console.log(
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
   port: Number(process.env.EMAIL_PORT),
-  secure: process.env.MAIL_SECURE === "false",
+  secure: process.env.EMAIL_SECURE === "true",
 
   auth: {
     user: process.env.EMAIL_USER,
